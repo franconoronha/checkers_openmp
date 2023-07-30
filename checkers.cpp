42d76756d363151e5ca0971164877d2555171e9d
franconoronha <fnpereira@inf.ufpel.edu.br>
2023-07-29 22:24:47 -0300

    base do jogo

---
+++ checkers.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <vector>
+#include "checkers.h"
+
+using namespace std;
+
+void clearScreen() {
+    cout << "\033[2J\033[1;1H";
+}
+
+int main() {
+    Board board = Board();
+    while(!board.isGameOver()) {
+        board.printBoard();
+        cout << "Enter the x coordinate of the piece you want to move: ";
+        int x;
+        cin >> x;
+        cout << "Enter the y coordinate of the piece you want to move: ";
+        int y;
+        cin >> y;
+        Square square = board.getSquare(x, y);
+        if(square.getPiece() == EMPTY) {
+            cout << "There is no piece there!" << endl;
+            continue;
+        }
+        if(board.getTurn() == BLACK_TURN && square.getPiece() != BLACK
+         || board.getTurn() == WHITE_TURN && square.getPiece() != WHITE) {
+            cout << "You can't move that piece!" << endl;
+            continue;
+        }
+        cout << "Enter the x coordinate of the square you want to move to: ";
+        int newX;
+        cin >> newX;
+        cout << "Enter the y coordinate of the square you want to move to: ";
+        int newY;
+        cin >> newY;
+        Square newSquare = board.getSquare(newX, newY);
+        if(newSquare.getPiece() != EMPTY) {
+            cout << "There is already a piece there!" << endl;
+            continue;
+        }
+        if(newX == x + 1 || newX == x - 1) {
+            if(newY == y + 1 || newY == y - 1) {
+                newSquare.setPiece(square.getPiece());
+                board.setSquare(newSquare, newX, newY);
+                board.setSquare(Square(x, y), x, y);
+                board.toggleTurn();
+            }
+            else {
+                cout << "You can't move there!" << endl;
+                continue;
+            }
+        }
+        else {
+            cout << "You can't move there!" << endl;
+            continue;
+        }
+    }
+    return 0;
+}
+
+void Board::printBoard()
+{
+    cout << " |0|1|2|3|4|5|6|7|" << endl;
+    cout << "-----------------" << endl;
+    for(int i = 0; i < 8; ++i) {
+        cout << i << "|";
+        for(int j = 0; j < 8; ++j) {
+            switch(squares[i][j].getPiece()) {
+                case WHITE:
+                    cout << "x ";
+                    break;
+                case BLACK:
+                    cout << "o ";
+                    break;
+                case WHITE_KING:
+                    cout << "X ";
+                    break;
+                case BLACK_KING:
+                    cout << "O ";
+                    break;
+                case EMPTY:
+                    cout << "- ";
+                    break;
+            }
+        }
+        cout << endl;    
+    }
+}
+
+void Board::resetBoard() {
+    for(int i = 0; i < 8; ++i) {
+        for(int j = 0; j < 8; ++j) {
+            if((i == 0 || i == 2) && j % 2 != 0) {
+                squares[i][j].setPiece(BLACK);
+                blackPieces.push_back(squares[i][j]);
+            }
+            if(i == 1 && j % 2 == 0) {
+                squares[i][j].setPiece(BLACK);
+                blackPieces.push_back(squares[i][j]);
+            }
+            if((i == 5 || i == 7) && j % 2 == 0) {
+                squares[i][j].setPiece(WHITE);
+                whitePieces.push_back(squares[i][j]);
+            }
+            if(i == 6 && j % 2 != 0) {
+                squares[i][j].setPiece(WHITE);
+                whitePieces.push_back(squares[i][j]);
+            }
+        }
+    }
+}
+
+void Board::toggleTurn() {
+    if(turn == BLACK_TURN) {
+        turn = WHITE_TURN;
+    }
+    else {
+        turn = BLACK_TURN;
+    }
+}
---
+++ checkers.h
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+enum Piece
+{
+    WHITE,
+    BLACK,
+    WHITE_KING,
+    BLACK_KING,
+    EMPTY
+};
+
+enum Turn
+{
+    BLACK_TURN,
+    WHITE_TURN
+};
+
+class Square
+{
+    Piece piece;
+    int x, y;
+
+public:
+    void setPiece(Piece newPiece) { piece = newPiece; }
+    Piece getPiece() { return piece; }
+    void setX(int newX) { x = newX; }
+    void setY(int newY) { y = newY; }
+    int getX() { return x; }
+    int getY() { return y; }
+    Square(Piece newPiece, int newX, int newY)
+    {
+        piece = newPiece;
+        x = newX;
+        y = newY;
+    };
+    Square(int newX, int newY)
+    {
+        piece = EMPTY;
+        x = newX;
+        y = newY;
+    };
+    Square() {
+        piece = EMPTY;
+        x = 0;
+        y = 0;
+    }
+};
+
+class Board
+{
+    Square squares[8][8];
+    bool gameOver;
+    Turn turn;
+
+public:
+    vector<Square> whitePieces;
+    vector<Square> blackPieces;
+    bool isGameOver() { return gameOver; }
+    void setGameOver(bool gameState) { gameOver = gameState; }
+    Turn getTurn() { return turn; }
+    void setTurn(Turn newTurn) { turn = newTurn; }
+    Board()
+    {
+        gameOver = false;
+        turn = WHITE_TURN;
+        resetBoard();
+    }
+    Square getSquare(int x, int y)
+    {
+        return squares[x][y];
+    }
+    void setSquare(Square square, int x, int y)
+    {
+        squares[x][y] = square;
+    }
+    void resetBoard();
+    void printBoard();
+    void toggleTurn();
+};
